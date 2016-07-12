7fc70730fc9b632c4ccfe61836d3e17a729a5170
Cristian Prieto <me@cprieto.com>
2016-07-12 15:14:29 +1000

    Add project files.

---
+++ NetDetector/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.Win32;
+
+// NOTE: Taken from https://msdn.microsoft.com/en-us/library/hh925567(v=vs.110).aspx
+namespace NetDetector
+{
+    public class GetUpdateHistory
+    {
+        public static void Main()
+        {
+            using (RegistryKey baseKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32).OpenSubKey(@"SOFTWARE\Microsoft\Updates"))
+            {
+                foreach (string baseKeyName in baseKey.GetSubKeyNames())
+                {
+                    if (baseKeyName.Contains(".NET Framework") || baseKeyName.StartsWith("KB") || baseKeyName.Contains(".NETFramework"))
+                    {
+
+                        using (RegistryKey updateKey = baseKey.OpenSubKey(baseKeyName))
+                        {
+                            string name = (string)updateKey.GetValue("PackageName", "");
+                            Console.WriteLine(baseKeyName + "  " + name);
+                            foreach (string kbKeyName in updateKey.GetSubKeyNames())
+                            {
+                                using (RegistryKey kbKey = updateKey.OpenSubKey(kbKeyName))
+                                {
+                                    name = (string)kbKey.GetValue("PackageName", "");
+                                    Console.WriteLine("  " + kbKeyName + "  " + name);
+
+                                    if (kbKey.SubKeyCount > 0)
+                                    {
+                                        foreach (string sbKeyName in kbKey.GetSubKeyNames())
+                                        {
+                                            using (RegistryKey sbSubKey = kbKey.OpenSubKey(sbKeyName))
+                                            {
+                                                name = (string)sbSubKey.GetValue("PackageName", "");
+                                                if (name == "")
+                                                    name = (string)sbSubKey.GetValue("Description", "");
+                                                Console.WriteLine("    " + sbKeyName + "  " + name);
+
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+}
